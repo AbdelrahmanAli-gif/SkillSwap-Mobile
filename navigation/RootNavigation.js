793b2614e8a674b7d87e97499268ba2936cd59e0
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-07-21 11:45:40 +0300

    fix(status bar): fix swith to arabic button covering part of the status bar

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -1,38 +1,54 @@
-import { Button, View } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useTranslation } from 'react-i18next';
-import { AuthProvider } from '../contexts/AuthContext';
-import LoginScreen from '../screens/LoginScreen';
-import RegisterScreen from '../screens/RegisterScreen';
-import AppNavigation from './AppNavigation';
-import MessagesScreen from '../screens/MessagesScreen';
+import { Button, View } from "react-native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { useTranslation } from "react-i18next"
+import { AuthProvider } from "../contexts/AuthContext"
+import LoginScreen from "../screens/LoginScreen"
+import RegisterScreen from "../screens/RegisterScreen"
+import AppNavigation from "./AppNavigation"
+import MessagesScreen from "../screens/MessagesScreen"
+import PictureBio from "../screens/RegisterSteps/PictureBio"
+import { SafeAreaView } from "react-native-safe-area-context"
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 const RootNavigation = () => {
-    const { i18n } = useTranslation();
+  const { i18n } = useTranslation()
 
-    const switchLang = async () => {
-        const newLang = i18n.language === "en" ? "ar" : "en";
-        await i18n.changeLanguage(newLang);
-    };
+  const switchLang = async () => {
+    const newLang = i18n.language === "en" ? "ar" : "en"
+    await i18n.changeLanguage(newLang)
+  }
 
-    return (
-        <View style={{ flex: 1, paddingTop: 30 }}>
-            <Button
-                title={i18n.language === "en" ? "Switch to Arabic" : "التبديل إلى الإنجليزية"}
-                onPress={switchLang}
-            />
-            <AuthProvider>
-                <Stack.Navigator screenOptions={{ title: "SkillSwap", headerTitleAlign: "center", headerBackVisible: false }}>
-                    <Stack.Screen name="Login" component={LoginScreen} />
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Button
+        title={i18n.language === "en" ? "Switch to Arabic" : "التبديل إلى الإنجليزية"}
+        onPress={switchLang}
+      />
+      <AuthProvider>
+        <Stack.Navigator
+          screenOptions={{
+            title: "SkillSwap",
+            headerTitleAlign: "center",
+            headerBackVisible: false,
+          }}
+        >
+          {/* <Stack.Screen name="Login" component={LoginScreen} />
                     <Stack.Screen name='Messages' component={MessagesScreen}></Stack.Screen>
                     <Stack.Screen name="Register" component={RegisterScreen} />
-                    <Stack.Screen name="App" options={{ headerShown: false }} component={AppNavigation} />
-                </Stack.Navigator>
-            </AuthProvider>
-        </View>
-    );
+                    <Stack.Screen name="App" options={{ headerShown: false }} component={AppNavigation} /> */}
+          <Stack.Screen
+            name="PictureBio"
+            component={PictureBio}
+            options={{
+              title: "Tell us about yourself",
+              headerBackVisible: true,
+            }}
+          />
+        </Stack.Navigator>
+      </AuthProvider>
+    </SafeAreaView>
+  )
 }
 
-export default RootNavigation;
+export default RootNavigation
---
+++ screens/RegisterSteps/PictureBio.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+export default function PictureBio() {
+  return (
+    <View>
+      <Text>PictureBio</Text>
+    </View>
+  )
+}
