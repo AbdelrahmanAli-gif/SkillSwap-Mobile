ab2809229d7325fab8a04311c39b7e03f445546d
siefghazy <siefghazy5@gmail.com>
2025-07-21 08:53:12 +0300

    update message comp

--- components/Messages.js
+++ components/Messages.js
@@ -3,14 +3,14 @@ import { Image, StyleSheet, Text, View } from "react-native";
 
 const Messages = ({ userName, img, content, time }) => {
   return (
-    <View className="w-full flex-row mt-3  ">
+    <View className="w-full flex-row mt-5  ">
       <View className="w-9/12 flex-row">
-        <View >
+        <View className="mt-1" >
             <Image className="rounded-full w-[55px] h-[55px]" source={img? require(`../assets/u1.png`):require("../assets/icon.png")}></Image>
         </View>
         <View className="ml-3 flex">
             <Text className="font-medium text-lg">{userName}</Text>
-            <Text numberOfLines={2} className="text-slate-500">{content}</Text>
+            <Text numberOfLines={1} className="text-slate-500">{content}</Text>
         </View>
         <View className="w-1/12 ">
         <Text>{time}</Text>
