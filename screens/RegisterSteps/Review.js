52207713f0807f5ae323aaae8b517741a434fa03
AbdelrahmanAli-gif <abdelrahmanali58@gmail.com>
2025-07-23 17:57:10 +0300

    (fix.) show name in review screen

--- screens/RegisterSteps/Review.js
+++ screens/RegisterSteps/Review.js
@@ -1,12 +1,15 @@
 import { Image, ScrollView, Text, View } from 'react-native';
+import { useAuth } from '../../contexts/AuthContext';
 import Button from '../../components/Button';
 
 const Review = ({ info }) => {
+    const { user } = useAuth();
+
     return (
         <ScrollView className="flex-1 px-5">
             <View className="flex items-center justify-center py-2 px-4 rounded-xl mt-2">
                 <Image source={require("../../assets/avatar.png")} className="w-28 h-28 rounded-full" />
-                <Text className="text-2xl font-bold mt-2">John Doe</Text>
+                <Text className="text-2xl font-bold mt-2">{user.name}</Text>
             </View>
             <View className="my-2">
                 <Text className="text-lg font-bold mt-2">About</Text>
